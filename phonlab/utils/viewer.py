f8ef0e279b214e0741cb89b42987ca91044d3bb8
Martin Oberg <obergma@gmail.com>
2025-11-27 16:01:47 -0800

    wip

--- phonlab/utils/viewer.py
+++ phonlab/utils/viewer.py
@@ -8,40 +8,26 @@ from matplotlib.patches import Rectangle
 import subprocess
 from .signal import loadsig
 from phonlab import prep_audio
-# from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+from phonlab import sgram
 
-def action():
-    print("hello")
-
-
-def on_click(event):
-    if event.button is MouseButton.LEFT:
-        print("clic")
-
-# class PlayAudio:
-#     def prev(self, event):
-#         print("play prev audio")
-#     def cur(self, event):
-#         print("play cur audio")
-#     def next(self, event):
-#         print("play next audio")
-#
-# callback = PlayAudio()
 
 class Viewer:
     def __init__(self, fn):
         self.start_x = None
         self.current_span = None
-        self.fig, self.ax = plt.subplots()
+        self.fig, self.axs = plt.subplots(nrows=3, ncols=1)
         self.fn = fn
         wavdata,fs = loadsig(fn, chansel=[0])  # taking just the left channel, with 'chansel'
-        self.ax.plot(np.arange(0, wavdata.size)/fs, wavdata)
-        self.ax.set_xlim([0,wavdata.size/fs])
+        self.axs[0].plot(np.arange(0, wavdata.size)/fs, wavdata)
+        self.axs[0].set_xlim([0,wavdata.size/fs])
+        sgram(wavdata, ax=self.axs[0])
+
+
         cid_press = self.fig.canvas.mpl_connect('button_press_event', self.on_press)
         cid_motion = self.fig.canvas.mpl_connect('motion_notify_event', self.on_motion)
         cid_release = self.fig.canvas.mpl_connect('button_release_event', self.on_release)
 
-        l, b, w, h =  self.ax.get_position().extents
+        l, b, w, h =  self.axs[0].get_position().extents
         print(l, b, w, h)
         self.axprev = self.fig.add_axes([l, b, 0, 0.05])
         self.axcur = self.fig.add_axes([l, b, w-l,  0.05])
@@ -55,7 +41,7 @@ class Viewer:
         plt.show()
 
     def play_segment(self, event):
-        xlims = self.ax.get_xlim()
+        xlims = self.axs[0].get_xlim()
         s1 = self.current_span.get_x()
         s2 = s1+self.current_span.get_width()
         if event.inaxes == self.axprev:
@@ -78,8 +64,8 @@ class Viewer:
         subprocess.run(["ffplay", "-loglevel", "quiet", "-ss", f"{start_time}", "-t", f"{end_time}", "-nodisp", f"-autoexit", f"{self.fn}"])
 
     def resize_play_buttons(self):
-        l, b, w, h =  self.ax.get_position().extents
-        xsz = np.diff(self.ax.get_xlim())[0]
+        l, b, w, h =  self.axs[0].get_position().extents
+        xsz = np.diff(self.axs[0].get_xlim())[0]
         s1 = self.current_span.get_x()
         s2 = s1+self.current_span.get_width()
         p1 =  ((s1/xsz)* (w-l))
@@ -92,7 +78,7 @@ class Viewer:
 
     def on_press(self, event):
         """Record the starting x-coordinate on button press."""
-        if event.inaxes != self.ax: return
+        if event.inaxes != self.axs[0]: return
         # Remove previous span if exists to allow drawing a new one
         if self.current_span:
             self.current_span.remove()
@@ -101,7 +87,7 @@ class Viewer:
 
     def on_motion(self, event):
         """Dynamically update the axvspan as the mouse moves (dragging)."""
-        if self.start_x is None or event.inaxes != self.ax: return
+        if self.start_x is None or event.inaxes != self.axs[0]: return
 
         # Remove the previous temporary span before drawing the new one
         if self.current_span:
@@ -109,13 +95,13 @@ class Viewer:
 
         end_x = event.xdata
         # Draw the new temporary vertical span
-        self.current_span = self.ax.axvspan(min(self.start_x, end_x), max(self.start_x, end_x), color='gray', alpha=0.5)
+        self.current_span = self.axs[0].axvspan(min(self.start_x, end_x), max(self.start_x, end_x), color='gray', alpha=0.5)
         self.resize_play_buttons()
         self.fig.canvas.draw_idle() # Redraw the canvas efficiently
 
     def on_release(self, event):
         """Finalize the axvspan on button release."""
-        if self.start_x is None or event.inaxes != self.ax: 
+        if self.start_x is None or event.inaxes != self.axs[0]: 
             return
         # self.selection_span.get_xy()[i, 0] = new_xmin if i in [0, 3] else new_xmax
         # s1 = self.current_span.get_x()
